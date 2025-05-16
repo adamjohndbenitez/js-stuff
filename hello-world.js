af0e0ec68e882c6660bed777c30ab5804b0c3d74
aj-datastealth <abenitez@datastealth.io>
2025-05-16 03:50:03 -0400

    [#9] Coursera - Introduction to Javascript
    - typeof, error handling, defensive progrogramming

--- hello-world.js
+++ hello-world.js
@@ -42,18 +42,121 @@ counter += 5;
 counter += 3;
 console.log(counter); // 8
 
-// 
+// advance operators: if-else statement
 var age = 10;
 if (age >= 65) {
    console.log('You get your income from your pension')
-} else {
-   console.log('not retired yet')
+} else if (age < 65 && age >= 18) {
+   console.log('Each month you get a salary')
+} else if (age < 18) {
+   console.log('You get an allowance')
 }
 
-// 
-var age = 10;
-if (age >= 65) {
-   console.log('You get your income from your pension')
-} else if (age < 65 || age >= 18) {
-   console.log('Each month you get a salary')
+// advance operators: switch statement
+var day = 'Sunday';
+switch(day) {
+   case 'Monday':
+       console.log('Read a book');
+       break;
+   case 'Tuesday':
+       console.log('Watch a movie');
+       break;
+   case 'Wednesday':
+       console.log('Read a book');
+       break;
+   case 'Thursday':
+       console.log('Play basketball');
+       break;
+   case 'Friday':
+       console.log('Socialize');
+       break;
+   case 'Saturday':
+       console.log('Chill');
+       break;
+   case 'Sunday':
+       console.log('Have barbecue');
+       break;
+   default:
+       //this block will run if no condition matches
+       console.log('There is no such day');
 }
+
+// Function
+function letterFinder(word, match) {
+    for(var i = 0; i < word.length; i++) {
+        if(word[i] == match) {
+            //check if the current characater, word[i], is equal to the match
+            console.log('Found the', match, 'at', i)
+        } else {
+            console.log('---No match found at', i)
+        }
+    }
+}
+
+letterFinder("test", "t")
+
+// datatype using typeof operator
+console.log("typeof typeof('what is this?')) "+typeof('what is this?'))
+console.log("typeof typeof(10)) "+typeof(10))
+console.log("typeof typeof(3.14)) "+typeof(3.14))
+console.log("typeof typeof(true)) "+typeof(true))
+console.log("typeof typeof(false)) "+typeof(false))
+console.log("typeof typeof(false)) "+typeof(1 < 2))
+console.log("typeof typeof([1,2,3])) "+typeof([1,2,3]))
+
+console.log("typeof typeof({firstProperty:1})) "+typeof({firstProperty:1}))
+
+// error prevention handling
+function addTwoNums(a,b) {
+    try {
+        if(typeof(a) != 'number') {
+            throw new ReferenceError('the first argument is not a number')
+        } else if (typeof(b) != 'number') {
+            throw new ReferenceError('the second argument is not a number')
+        } else {
+            console.log(a + b)
+        }
+    } catch(err) {
+        console.log("Error!", err)
+    }
+}
+addTwoNums(5, "5")
+console.log("It still works")
+
+
+// array and object
+var clothes = [];
+clothes.push('gray t-shirt'); // 1st item of clothing
+clothes.push('blue t-shirt'); // 2nd item of clothing
+clothes.push('yellow t-shirt'); // 3rd item of clothing
+clothes.push('slippers'); // 4th item of clothing
+clothes.push('old jeans'); // 5th item of clothing
+clothes.pop();
+clothes.push('green scarf');
+console.log(clothes[2]);
+
+var favCar = {};
+favCar.color = "red";
+favCar.convertible = true;
+console.log(favCar);
+
+// Defensive programming
+function letterFinder(word, match) {
+    var condition1 = typeof(word) == 'string' && word.length >= 2; //if the word is a string and the length is greater than or equal to 2
+    var condition2 = typeof(match) == 'string' && match.length == 1; //if the match is a string and the length is equal to 1
+    if(condition1 && condition2) { //if both condition matches
+        for(var i = 0; i < word.length; i++) {
+            if(word[i] == match) {
+                //check if the character at this i position in the word is equal to the match
+                console.log('Found the', match, 'at', i)
+            } else {
+                console.log('---No match found at', i)
+            }
+        }
+    } else {
+        //if the requirements don't match
+        console.log("Please pass correct arguments to the function")
+    }
+}
+letterFinder([],[])
+letterFinder("cat","c")
